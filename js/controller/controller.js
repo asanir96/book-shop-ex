7dfbafdebe8c951a2d39ea51ed89b3400be8057b
asanir96 <asanir96@gmail.com>
2026-04-01 09:04:07 +0300

    Implement renderBooks

--- js/controller/controller.js
+++ js/controller/controller.js
@@ -1,9 +1,35 @@
 'use strict'
+var gBooks
 
-function onInit(){
+function onInit() {
     renderBooks()
 }
 
-function renderBooks(){
-    console.log('renderBooks')
+function renderBooks() {
+    gBooks = getBooks()
+    console.log(gBooks)
+
+    const elTable = document.querySelector('table')
+
+    var tableStrHTML = `<table>
+                        <tr>
+                            <th>Title</th>
+                            <th>Price</th>
+                            <th>Actions</th>
+                        </tr>`
+
+    tableStrHTML += gBooks.map(book => {
+        return `<tr>
+            <td>${book.price}</td>
+            <td>${book.title}</td>
+            <td>
+                <button class="read-btn">Read</button>
+                <button class="update-btn">Update</button>
+                <button class="delete-btn">Delete</button>
+
+            </td>
+        </tr>`
+    }).join('')
+
+    elTable.innerHTML = tableStrHTML
 }
--- js/services/book.service.js
+++ js/services/book.service.js
@@ -23,5 +23,5 @@ var gBooks = [
 ]
 
 function getBooks() {
-    console.log('hi')
+    return gBooks
 }
