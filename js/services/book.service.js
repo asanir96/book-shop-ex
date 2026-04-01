5c67655bc4a5af97db6e3561e5878d4ac64d7630
asanir96 <asanir96@gmail.com>
2026-04-01 08:41:21 +0300

    Init Repo

---
+++ js/services/book.service.js
@@ -0,0 +1,27 @@
+'use strict'
+
+var gBooks = [
+    {
+        id: 1,
+        title: 'Harry Potter and the Order of Phoenix',
+        price: 120,
+        imgUrl: 'lori-ipsi.jpg'
+    },
+    {
+        id: 2,
+        title: 'The Hobbit',
+        price: 160,
+        imgUrl: 'lori-ipsi.jpg'
+    },
+    {
+        id: 3,
+        title: 'The Martian',
+        price: 80,
+        imgUrl: 'lori-ipsi.jpg'
+    }
+
+]
+
+function getBooks() {
+    console.log('hi')
+}
